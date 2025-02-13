09dbcce726ab74c7c43b0738eb789bc2a55e237c
Lap76 <120988019+Lap76@users.noreply.github.com>
2025-02-13 19:51:51 +0100

    Update serverInit.lua
    Fixes the "Couldn't determine your OS! Are your running on steroids??" error with help from ChatGPT

--- server/serverInit.lua
+++ server/serverInit.lua
@@ -1,50 +1,148 @@
 kjxmlData = {}
+local systemOS = nil
+
+local function parseObjSet(data, fileName)
+    local xml = SLAXML:dom(data)
+
+    if xml and xml.root and xml.root.name == 'vcfroot' then ParseVCF(xml, fileName) end
+end
+
+local function checkForUpdate()
+    local versionFile = LoadResourceFile(GetCurrentResourceName(), 'version.json')
+
+    if not versionFile then
+        print("Couldn't read version file!")
+        return
+    end
+
+    local currentVersion = Semver(json.decode(versionFile)['version'] or 'unknown')
+
+    if currentVersion.prerelease then
+        print('WARNING: you are using a pre-release of MISS ELS!')
+        print('>> This version might be unstable and probably contains some bugs.')
+        print('>> Please report bugs and other problems via https://github.com/matsn0w/MISS-ELS/issues')
+    end
+
+    PerformHttpRequest('https://api.github.com/repos/matsn0w/MISS-ELS/releases/latest', function(status, response, headers)
+        if status ~= 200 then
+            print("Something went wrong! Couldn't fetch latest version. Status: " .. tostring(status))
+            return
+        end
+
+        local newVersion = Semver(json.decode(response)['tag_name'] or 'unknown')
+
+        if newVersion > currentVersion then
+            print('---------------------------- MISS ELS ----------------------------')
+            print('--------------------- NEW VERSION AVAILABLE! ---------------------')
+            print('>> You are using v' .. tostring(currentVersion))
+            print('>> You can upgrade to v' .. tostring(newVersion))
+            print()
+            print('Download it at https://github.com/matsn0w/MISS-ELS/releases/latest')
+            print('------------------------------------------------------------------')
+        end
+    end)
+end
+
+local function determineOS()
+    local osType = GetConvar("os_type", "")
+    
+    if osType and osType ~= "" then
+        return osType
+    end
+    
+    if os.getenv("HOME") then return "unix" end
+    if os.getenv("HOMEPATH") then return "windows" end
+
+    if package and package.config and package.config:sub(1,1) == "\\" then
+        return "windows"
+    else
+        return "unix"
+    end
+
+    error("Couldn't determine your OS! Please report this issue.")
+end
+
+local function scanDir(folder)
+    local pathSeparator = '/'
+    local command = 'ls -A'
+
+    if systemOS == 'windows' then
+        pathSeparator = '\\'
+        command = 'dir /R /B'
+    end
+
+    local resourcePath = GetResourcePath(GetCurrentResourceName())
+    local directory = resourcePath .. pathSeparator .. folder
+    local i, t, popen = 0, {}, io.popen
+    local pfile = popen(command .. ' "' .. directory .. '"')
+
+    for filename in pfile:lines() do
+        i = i + 1
+        t[i] = filename
+    end
+
+    if #t == 0 then
+        error("Couldn't find any VCF files. Are they in the correct directory?")
+    end
+
+    pfile:close()
+    return t
+end
+
+local function loadFile(file)
+    return LoadResourceFile(GetCurrentResourceName(), file)
+end
 
 AddEventHandler('onResourceStart', function(name)
-    if name:lower() == GetCurrentResourceName():lower() then
-	    for i=1,#Config.ELSFiles do
-			local data = LoadResourceFile(GetCurrentResourceName(), "xmlFiles/" .. Config.ELSFiles[i])
-			
-		    if data then
-		        parseObjSet(data, Config.ELSFiles[i])
-		    end
-		end
-		
-		TriggerClientEvent("kjELS:sendELSInformation", -1, kjxmlData)
-	end
-end)
+    if not Config then
+        error('You probably forgot to copy the example configuration file. Please see the installation instructions for further details.')
+        StopResource(GetCurrentResourceName())
+        CancelEvent()
+        return
+    end
 
-RegisterServerEvent("kjELS:requestELSInformation")
+    if name:lower() ~= GetCurrentResourceName():lower() then
+        CancelEvent()
+        return
+    end
 
-AddEventHandler("kjELS:requestELSInformation", function()
-	TriggerClientEvent("kjELS:sendELSInformation", source, kjxmlData)
-end)
+    Citizen.CreateThread(function()
+        checkForUpdate()
+    end)
 
-function parseObjSet(data, fileName)
-    local xml = SLAXML:dom(data, fileName)
-    if xml and xml.root then
-        if xml.root.name == "vcfroot" then
-            parseVehData(xml, fileName)
+    local folder = 'xmlFiles'
+
+    -- Bepaal het besturingssysteem van de server
+    systemOS = determineOS()
+
+    if not systemOS then
+        error("Couldn't determine your OS! Please report this issue.")
+    end
+
+    for _, file in pairs(scanDir(folder)) do
+        local data = loadFile(folder .. '/' .. file)
+
+        if data then
+            if pcall(function() parseObjSet(data, file) end) then
+                print('Parsed VCF for: ' .. file)
+            else
+                print('VCF file ' .. file .. ' could not be parsed: is your XML valid?')
+            end
+        else
+            print('VCF file ' .. file .. ' not found: does the file exist?')
         end
     end
-end
 
-RegisterNetEvent("baseevents:enteredVehicle")
-AddEventHandler("baseevents:enteredVehicle", function(veh, seat, name)
-    TriggerClientEvent('kjELS:initVehicle', source)
+    -- Stuur de ELS-data naar alle clients
+    TriggerClientEvent('kjELS:sendELSInformation', -1, kjxmlData)
 end)
 
-function sendToDiscord(color, name, message, footer)
-    local embed = {
-          {
-              ["color"] = color,
-              ["title"] = "",
-              ["description"] = message,
-              ["footer"] = {
-                  ["text"] = footer,
-              },
-          }
-      }
-  
-    PerformHttpRequest('https://discordapp.com/api/webhooks/773656962746417202/czAx1DxypRUuutdykxiOCrK6k_0nSHe4i5BxD2VaDWirg_wlKbOR6LJnfRSkXlDfRTaK', function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
-end
+RegisterServerEvent('kjELS:requestELSInformation')
+AddEventHandler('kjELS:requestELSInformation', function()
+    TriggerClientEvent('kjELS:sendELSInformation', source, kjxmlData)
+end)
+
+RegisterNetEvent('baseevents:enteredVehicle')
+AddEventHandler('baseevents:enteredVehicle', function(veh, seat, name)
+    TriggerClientEvent('kjELS:initVehicle', source)
+end)
